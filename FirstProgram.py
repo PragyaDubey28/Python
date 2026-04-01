e70f7419b7807719762e008d6ca933a78c36b4f4
Pragya <pragyadubey313@gmail.com>
2026-04-01 15:23:43 +0530

    First Python Code

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,30 +1,30 @@
-# print("Hello World")
-# print("My name is pragya")
+print("Hello World")
+print("My name is pragya")
 
 
 #Veriable
 
-# name ="Pragya"
-# age = 25
-# price = 25.99
+name ="Pragya"
+age = 25
+price = 25.99
 
-# print(type(name))
-# print(type(age))
-# print(type(price))
+print(type(name))
+print(type(age))
+print(type(price))
 
-# age2 = age
-# print(age2)
+age2 = age
+print(age2)
 
-# print("My name is :", name )
-# print("My age is :", age )
+print("My name is :", name )
+print("My age is :", age )
+
 
 
 #  Print Sum
-
-# a = 2000
-# b = 355 
-# sum = a + b
-# print(sum)
+a = 2000
+b = 355 
+sum = a + b
+print(sum)
 
 
 
@@ -32,56 +32,73 @@
 
 # Arithmetic Operators
 
-# a = 6
-# b = 2
-#   #a^b
+a = 6
+b = 2
+  #a^b
 
   
 
 # Relational / Comparision Operator
 
-# a = 50
-# b = 20
+a = 50
+b = 20
 
-# print(a == b) #False
-# print(a !=b)  #True
-# print(a >= b)
-# print(a > b)
-# print(a >= b)
-# print(a < b)
-# print(a <= b)
+print(a == b) #False
+print(a !=b)  #True
+print(a >= b)
+print(a > b)
+print(a >= b)
+print(a < b)
+print(a <= b)
 
 
 
 
 #Assignment Operator
-# num = 10
-# num -=10 
-# num *=10 
-# num /=10 
-# num %=10 
-# print("num :", num)
+num = 10
+num -=10 
+num *=10 
+num /=10 
+num %=10 
+print("num :", num)
 
 
 
 
 # Input in Python
 
-# input("enter your name: ")
+input("enter your name: ")
 
 
 
-# name = input("enter your name: ")
-# print("Welcome ", name)
+name = input("enter your name: ")
+print("Welcome ", name)
 
 
-# name = input("enter your age: ")
-# print("You entered ", name)
+name = input("enter your age: ")
+print("You entered ", name)
 
-# val = input("enter some value: ")
-# print(type(val), val)
+val = input("enter some value: ")
+print(type(val), val)
 
-# name = input("enter name: ")
-# age = input("enter age: ")
-# marks = input("enter marks: ")
+name = input("enter name: ")
+age = input("enter age: ")
+marks = input("enter marks: ")
 
+
+# Practics Question
+
+
+
+print("area =", side * side)
+
+
+a = float(input("enter first : "))
+b = float(input("enter second : "))
+print("average=",  (a+b)/ 2 )
+
+
+a = int(input("enter first : "))
+b = int(input("enter second : "))
+
+print(a >= b)
