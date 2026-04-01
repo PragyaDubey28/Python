e1f02dad882a2a9e5d650a167dfd2783ec19f915
Pragya <pragyadubey313@gmail.com>
2026-04-01 15:01:15 +0530

    first commit

---
+++ FirstProgram.py
@@ -0,0 +1,87 @@
+# print("Hello World")
+# print("My name is pragya")
+
+
+#Veriable
+
+# name ="Pragya"
+# age = 25
+# price = 25.99
+
+# print(type(name))
+# print(type(age))
+# print(type(price))
+
+# age2 = age
+# print(age2)
+
+# print("My name is :", name )
+# print("My age is :", age )
+
+
+#  Print Sum
+
+# a = 2000
+# b = 355 
+# sum = a + b
+# print(sum)
+
+
+
+# Types of Operator
+
+# Arithmetic Operators
+
+# a = 6
+# b = 2
+#   #a^b
+
+  
+
+# Relational / Comparision Operator
+
+# a = 50
+# b = 20
+
+# print(a == b) #False
+# print(a !=b)  #True
+# print(a >= b)
+# print(a > b)
+# print(a >= b)
+# print(a < b)
+# print(a <= b)
+
+
+
+
+#Assignment Operator
+# num = 10
+# num -=10 
+# num *=10 
+# num /=10 
+# num %=10 
+# print("num :", num)
+
+
+
+
+# Input in Python
+
+# input("enter your name: ")
+
+
+
+# name = input("enter your name: ")
+# print("Welcome ", name)
+
+
+# name = input("enter your age: ")
+# print("You entered ", name)
+
+# val = input("enter some value: ")
+# print(type(val), val)
+
+# name = input("enter name: ")
+# age = input("enter age: ")
+# marks = input("enter marks: ")
+
