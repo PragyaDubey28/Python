8b5a9bd1b8c69fea84d8e92897f3bfba3fba8e17
Pragya <pragyadubey313@gmail.com>
2026-04-07 22:28:35 +0530

    Project

---
+++ Calculator.py
@@ -0,0 +1,15 @@
+# Calculator Project
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+op = input("Enter operator (+, -, *, /): ")
+
+if op == "+":
+    print(a + b)
+elif op == "-":
+    print(a - b)
+elif op == "*":
+    print(a * b)
+elif op == "/":
+    print(a / b if b != 0 else "Cannot divide by zero")
+else:
+    print("Invalid operator")
---
+++ List truple.py
@@ -0,0 +1,63 @@
+# marks = [94.2, 84.6, 95.6, 54.6, 38.9] 
+# print(marks)
+# print(type(marks))
+# print(len(marks))
+# print(marks[0])
+# print(marks[1])
+
+
+# student = ["Pragya", 95.2, 18, "Gorakhpur"]
+# print(student[0])
+# student[0] = "Piyush"
+# print(student)
+
+# list
+#list.append
+# list = [2, 3 , 1]
+# list.append(4)
+# print(list)
+
+#list.sort
+
+# list = [8, 3, 5, 1]
+# print(list.sort())
+# print(list)
+
+#list.sort(reverse=True)
+
+# list = [8, 3, 5, 1]
+# print(list.sort(reverse=True))
+# print(list)
+
+
+
+# list = ["a", "b", "c", "d"]
+# print(list.sort)
+# print(list.sort(reverse=True))
+# print(list)
+
+#list.reverse
+
+# list = [8, 3, 5, 1]
+# print(list.reverse())
+# print(list)
+
+#list.insert
+
+
+# list = [2, 1, 3]
+# list.insert(1, 5)
+# print(list)
+ 
+# list = [5, 8, 2, 3]
+# list.insert(2, 6)
+# print(list)
+
+#list.remove
+
+list = [2, 1, 3, 1]
+list.pop(2)
+print(list)
+
+# Tuples
+
